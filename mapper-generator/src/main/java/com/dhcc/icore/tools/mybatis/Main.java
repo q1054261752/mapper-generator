521378be83b4a8fb7ceb3296ed60c6b86144c7e0
zhangkairong <1054261752@qq.com>
2018-02-19 12:18:24 +0800

    在Jar包的同一目录下，加载配置文件

--- mapper-generator/src/main/java/com/dhcc/icore/tools/mybatis/Main.java
+++ mapper-generator/src/main/java/com/dhcc/icore/tools/mybatis/Main.java
@@ -2,6 +2,9 @@ package com.dhcc.icore.tools.mybatis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 import org.apache.ibatis.io.Resources;
@@ -26,9 +29,22 @@ public class Main {
 			throw new RuntimeException("文件yml不能为空!");
 		}
 
+		File f = new File(".");
+		StringBuilder sb = new StringBuilder();
+
+		String absolutePath = f.getAbsolutePath();
+		absolutePath = absolutePath.replace(".","");
+		sb.append(absolutePath);
+
+
 		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+
+		sb.append(args);
+		System.out.println(sb.toString());
+
 		try {
-			InputStream inputStream=Resources.getResourceAsStream(args);
+//			InputStream inputStream=Resources.getResourceAsStream(sb.toString());
+			InputStream inputStream = new FileInputStream(sb.toString());
 			GeneratorConfig config = mapper.readValue(inputStream, GeneratorConfig.class);
 			config.afterPropertiesSet();
 			MybatisMysqlMapperGenerator generator = new MybatisMysqlMapperGenerator(config);
